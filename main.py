ff630c1a7e9134ece1c91f025ed52b5b7ac0c741
Toto <Toto@toto.com>
2022-09-08 15:32:23 +0200

    Modification fichier main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, send_file, redirect, url_for,
 
 app = Flask (__name__)
 
-import yes
+import sys
 import os
 
 @app.route('/', methods=['GET', 'POST'])
