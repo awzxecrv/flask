8839d409f3792c0e3047dbadfc542b8a6d0254f0
Toto <Toto@toto.com>
2022-09-08 16:10:07 +0200

    Modification fichier main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 from flask import Flask, render_template, request, send_file, redirect, url_for, Response, redirect
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 import sys
 import os
@@ -10,7 +9,6 @@ import os
 
 def Menu():
     return render_template("home.html")
-
 
 if __name__ == "__name__":
     app.run(debug=True, host='0.0.0.0', port=8001)
